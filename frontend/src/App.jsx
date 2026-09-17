f84d37490492d3b676b72c6d072a34a290f74e58
Abhijeet Salunke <abhijeetsalunke16@gmail.com>
2026-09-17 15:04:58 +0530

    second version added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -156,7 +156,7 @@ function App() {
     <div className="app">
       <header className="topbar">
         <div>
-          <div className="brand">🎓 StudentHub Version 1</div>
+          <div className="brand">🎓 StudentHub Version 2</div>
           <p>Student Management System</p>
         </div>
 
